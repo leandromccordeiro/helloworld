00b2dc21fdebe37a4a885e407e08335f37d7aab6
leandromccordeiro <leandroo_marcos@yahoo.com.br>
2024-02-21 23:08:55 -0300

    criando primeira requisição

---
+++ saudacao.php
@@ -0,0 +1,13 @@
+<?php
+// Verifica se o valor do nome foi enviado via POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtém o nome do formulário
+    $nome = htmlspecialchars($_REQUEST['nome']); // Limpa os dados de entrada para evitar ataques XSS
+    
+    if (empty($nome)) {
+        echo "Nome está vazio.";
+    } else {
+        echo "Olá, " . $nome . "! Seja bem-vindo(a).";
+    }
+}
+?>
